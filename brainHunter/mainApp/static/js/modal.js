72b580e7acfdd71ddb76e1091c048f6ff6f6145b
Konstantin Nered <konstantin.n616@gmail.com>
2024-11-30 02:01:47 +0700

    did some modal window code

--- brainHunter/mainApp/static/js/modal.js
+++ brainHunter/mainApp/static/js/modal.js
@@ -5,13 +5,14 @@ const sendApplicationBtn = document.querySelectorAll('#sendApplicationBtn');
 
 sendApplicationBtn.forEach((item) => {
     item.addEventListener('click', () => {
-        console.log(item);
+        let dataArray = (item.dataset.btnId).split(':');
+        console.log(dataArray)
     })
 })
 
-for (let i = 0; i < vacancyID.length; i++){
-    console.log(vacancyID[i].dataset.id);
-}
+// for (let i = 0; i < vacancyID.length; i++){
+//     console.log(vacancyID[i].dataset.id);
+// }
 
 closeModal.addEventListener('click', () => {
     modal.classList.toggle('hidden');
