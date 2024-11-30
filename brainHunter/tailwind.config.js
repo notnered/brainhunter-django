219a21ed1dea894e07984d1335688e10882461cf
Konstantin Nered <konstantin.n616@gmail.com>
2024-12-01 03:18:54 +0700

    compiled tailwind css

--- brainHunter/tailwind.config.js
+++ brainHunter/tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './mainApp/static/js/**/*.js',
+    './mainApp/static/css/**/*.css',
+    './mainApp/templates/**/*.html'
+  ],
   theme: {
     screens: {
       'sm': '576px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
     },
     extend: {},
   },
